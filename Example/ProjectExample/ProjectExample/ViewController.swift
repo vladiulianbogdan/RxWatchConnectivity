8cf30e9833d4c312ed5e39b71d55e44f1905c745
Bogdan Vlad <vladbgd@gmail.com>
2019-08-14 17:41:29 +0300

    Add example project

---
+++ Example/ProjectExample/ProjectExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectExample
+//
+//  Created by Bogdan Vlad on 8/14/19.
+//  Copyright © 2019 Bogdan Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
