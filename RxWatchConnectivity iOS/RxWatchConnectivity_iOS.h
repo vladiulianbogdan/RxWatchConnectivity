bf3ee23b52550d44d0bf909084c2038614a167aa
Bogdan Vlad <vladbgd@gmail.com>
2019-08-14 17:24:41 +0300

    Initial commit

---
+++ RxWatchConnectivity iOS/RxWatchConnectivity_iOS.h
@@ -0,0 +1,18 @@
+//
+//  RxWatchConnectivity_iOS.h
+//  RxWatchConnectivity iOS
+//
+//  Created by Bogdan Vlad on 8/13/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxWatchConnectivity_iOS.
+FOUNDATION_EXPORT double RxWatchConnectivity_iOSVersionNumber;
+
+//! Project version string for RxWatchConnectivity_iOS.
+FOUNDATION_EXPORT const unsigned char RxWatchConnectivity_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxWatchConnectivity_iOS/PublicHeader.h>
+
+
---
+++ RxWatchConnectivity watchOS/RxWatchConnectivity_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  RxWatchConnectivity_watchOS.h
+//  RxWatchConnectivity watchOS
+//
+//  Created by Bogdan Vlad on 8/13/19.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RxWatchConnectivity_watchOS.
+FOUNDATION_EXPORT double RxWatchConnectivity_watchOSVersionNumber;
+
+//! Project version string for RxWatchConnectivity_watchOS.
+FOUNDATION_EXPORT const unsigned char RxWatchConnectivity_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxWatchConnectivity_watchOS/PublicHeader.h>
+
+
